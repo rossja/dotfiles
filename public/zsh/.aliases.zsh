7ce91037beeea40af52e1c17666a003e48149684
Jason Ross <algorythm@gmail.com>
2018-11-28 17:16:53 -0500

    I did it for the lulz!

--- public/zsh/.aliases.zsh
+++ public/zsh/.aliases.zsh
@@ -1,26 +1,44 @@
-# ls
+# -------------------------------------------------
+# ls tweaks
+# -------------------------------------------------
 #alias ls='gls --color=auto' # requires coreutils on mac osx
 alias ll='ls -alhF'
 alias la='ls -A'
 alias l='ls -CF'
 alias l.='ls -la'
 
+# -------------------------------------------------
 # brew maintenance
+# -------------------------------------------------
 alias brewup='brew update; brew upgrade; brew prune; brew cleanup; brew doctor'
 
+# -------------------------------------------------
 # header curl (useful for web app testing)
+# -------------------------------------------------
 alias hcurl="curl -s -o /dev/null -L -D - -A 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'"
 
+# -------------------------------------------------
 # vscode handy helper
+# -------------------------------------------------
 alias edit='/usr/local/bin/code-insiders'
 
+# -------------------------------------------------
 # handy for getting to the dotfiles dirs quickly
+# -------------------------------------------------
 alias cdf='cd $HOME/Documents/dotfiles'
 alias cdfp='cd $HOME/Documents/dotfiles/public'
 
+# -------------------------------------------------
 # git fun
+# -------------------------------------------------
 alias yolo='git commit -a -m "$(curl -s https://cb.pe/yolo)" && git push'
 
+# -------------------------------------------------
 # docker stuff I find useful
+# -------------------------------------------------
+# vault things
 alias vault="docker exec -i $(docker ps|awk '/vault server/ {print $1}') vault"
 alias file2vault="cat $1| vault kv put $2 value=-"
+
+# use app-specific mongo
+alias amongo="mongo -u $appDbUser -p appDbPass $appDbName"

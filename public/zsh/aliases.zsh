f63d03db50cce78acb68beef30f26e6f750047f2
rossja <algorythm@gmail.com>
2025-12-29 10:54:47 -0500

    cleaned things up

--- public/zsh/aliases.zsh
+++ public/zsh/aliases.zsh
@@ -42,18 +42,8 @@ alias privdot="cd $HOME/Documents/dotfiles/private"
 # -------------------------------------------------
 # git fun
 # -------------------------------------------------
-alias yolo='git commit -a -m "$(curl -s https://cb.pe/yolo)" && git push'
-alias guppy='git checkout master && git pull && git checkout json-dev && git pull'
+# alias yolo='git commit -a -m "$(curl -s https://whatthecommit.com/index.txt)" && git push'
 alias gitid='git rev-parse --short HEAD'
-
-# -------------------------------------------------
-# I hate typing
-# CLIENT_WORK_DIR is set in .zshrc.priv
-# -------------------------------------------------
-alias cdc="cd $CLIENT_WORK_DIR"
-alias cdcd="cd $CLIENT_WORK_DIR/docs"
-alias cdct="cd $CLIENT_WORK_DIR/testing"
-alias cdcs="cd $CLIENT_WORK_DIR/src"
 
 # -------------------------------------------------
 # docker stuff I find useful
@@ -65,7 +55,7 @@ alias dps="docker ps -a"
 # `docker build -t rails-toolbox -f Dockerfile.rails $(args) .`
 alias args='for var in `cat .env`; do out+="--build-arg ${var} "; done; echo ${out}; out=""'
 
-# vault things
+# docker vault things
 alias vault="docker exec -i $(docker ps|awk '/vault:latest/ {print $1}') vault"
 alias openvault="vault operator unseal $Unseal_Key_1; vault operator unseal $Unseal_Key_2; vault operator unseal $Unseal_Key_3"
 alias file2vault="cat $1| vault kv put $2 value=-" # arg1=filepath  arg2=vaultpath
@@ -77,12 +67,6 @@ alias dockershell="docker exec -it ${1} ${2}"
 # random other stuff
 # -------------------------------------------------
 
-# use app-specific mongo
-alias amongo="mongo -u $appDbUser -p appDbPass $appDbName"
-
-# bladeRF firmware load
-alias bladerunner="bladeRF-cli -l ~/.bladeRF/hostedx40-latest.rbf; bladeRF-cli -i"
-
 # get rid of command number and timestamp from history
 alias histrim="history | cut -c 26-"
 
@@ -93,22 +77,10 @@ alias randpw="openssl rand 60 | openssl base64 -A"
 alias gcpls='gcloud compute instances list | grep ${1}'
 
 # check ciphers and key lengths of SSH keys
-#export KEYSREGEX="${HOME}/.ssh/*.pub"
+export KEYSREGEX="${HOME}/.ssh/keys./*.pub"
 alias keyscan="for key in ${KEYSREGEX}; do echo -e \"\${key}\"; ssh-keygen -l -f \"\${key}\" 2> /dev/null; echo -e \"\\n\";  done | uniq"
 
-# microk8s aliases -- counterintuitive because `which` returns '1' if command not found
-#if [[ $(which kubectl) ]]; then
-#  alias kubectl='microk8s kubectl'
-#fi
-#if [[ $(which helm) ]]; then
-#  alias helm='microk8s helm3'
-#fi
-
-# homebrew python ftw
-#[[ -f /opt/homebrew/bin/python3 ]] && alias python=/opt/homebrew/bin/python3
-#[[ -f /opt/homebrew/bin/pip3 ]] && alias pip=/opt/homebrew/bin/pip3
-
-# after i figure out a command line trick i want to keep forever
+# for when i figure out a command line trick i want to keep forever
 alias keeper='echo $(history -p !!) >>~/.keeper.txt'
 
 # ag with ignore
